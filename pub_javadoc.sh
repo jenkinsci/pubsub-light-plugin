55a725113c8e4773527de5404bca0c989a6ff1c7
tfennelly <tom.fennelly@gmail.com>
2016-04-29 13:57:50 +0100

    Publish Javadoc script

---
+++ pub_javadoc.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+pushd target/site/apidocs
+
+git init
+git remote add javadoc git@github.com:tfennelly/jenkins-pubsub-bus-module.git
+git fetch --depth=1 javadoc gh-pages
+
+git add --all
+git commit -m "javadoc"
+git merge --no-edit -s ours remotes/javadoc/gh-pages
+
+git push javadoc master:gh-pages
+
+rm -rf .git
+
+popd
