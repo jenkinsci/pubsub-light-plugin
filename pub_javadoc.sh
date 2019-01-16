8e84fb5fbac0d639e5bd33e9f07c241483eba7e4
tfennelly <tom.fennelly@gmail.com>
2019-01-16 09:32:23 +0000

    fix pub_javadoc.sh

--- pub_javadoc.sh
+++ pub_javadoc.sh
@@ -5,7 +5,7 @@ mvn javadoc:javadoc
 pushd target/site/apidocs
 
 git init
-git remote add javadoc git@github.com:jenkinsci/pubsub-light-module.git
+git remote add javadoc git@github.com:jenkinsci/pubsub-light-plugin.git
 git fetch --depth=1 javadoc gh-pages
 
 git add --all
