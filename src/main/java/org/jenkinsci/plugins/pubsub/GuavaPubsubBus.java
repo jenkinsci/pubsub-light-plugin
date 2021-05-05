57a909830e8d92f9efd5b7218d3f5c4c39712bd1
olivier lamy <olamy@apache.org>
2021-05-06 09:22:08 +1000

    fix javadoc
    Signed-off-by: olivier lamy <olamy@apache.org>

--- src/main/java/org/jenkinsci/plugins/pubsub/GuavaPubsubBus.java
+++ src/main/java/org/jenkinsci/plugins/pubsub/GuavaPubsubBus.java
@@ -35,7 +35,6 @@ import javax.annotation.CheckForNull;
 import javax.annotation.Nonnull;
 import java.util.Map;
 import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
 import java.util.concurrent.LinkedBlockingQueue;
 import java.util.concurrent.ThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
